1d04a5b92d771e2f5b4829bd17319af7e625f02d
buscotoxica <osvaldo.moya@sansano.usm.cl>
2025-01-23 12:36:13 -0300

    cambios generales

--- web_control_usuario/settings.py
+++ web_control_usuario/settings.py
@@ -81,27 +81,27 @@ WSGI_APPLICATION = 'web_control_usuario.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE':'django.db.backends.postgresql_psycopg2',
-        'NAME':'departamento_sig_c',
-        'USER':'departamento_sig_c',
-        'PASSWORD':'Sigcontrol2023',
-        'HOST':'localhost',
-        'PORT':'5432',
-    }
-}
-
 # DATABASES = {
 #     'default': {
 #         'ENGINE':'django.db.backends.postgresql_psycopg2',
 #         'NAME':'departamento_sig_c',
-#         'USER':'postgres',
-#         'PASSWORD':'osval217K',
+#         'USER':'departamento_sig_c',
+#         'PASSWORD':'Sigcontrol2023',
 #         'HOST':'localhost',
 #         'PORT':'5432',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE':'django.db.backends.postgresql_psycopg2',
+        'NAME':'departamento_sig_c',
+        'USER':'postgres',
+        'PASSWORD':'osval217K',
+        'HOST':'localhost',
+        'PORT':'5432',
+    }
+}
 
 
 # Password validation
