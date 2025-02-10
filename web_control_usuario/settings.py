a8d9716b039e177492e395183321c4b02da1bee1
H-Emanuel <emanuelvperez2000@gmail.com>
2025-02-10 16:28:56 -0300

    cambio_setting

--- web_control_usuario/settings.py
+++ web_control_usuario/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'sz308p48$%9_v+&$*=d)gd2dak5*n-2-zr5989(c#d(r+9)h55'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.12.80','departamentosig.munivalpo.cl','localhost',"*",'5ae8-200-50-126-98.ngrok-free.app']
+ALLOWED_HOSTS = ['192.168.12.80','departamentosig.munivalpo.cl','localhost',"*",'5ae8-200-50-126-98.ngrok-free.app','www.departamentosig.munivalpo.cl']
 
 CORS_ALLOWED_ORIGINS = [
     "5ae8-200-50-126-98.ngrok-free.app",
