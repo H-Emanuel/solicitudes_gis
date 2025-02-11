481e024f72e3b87922d7153693aabfdf848173b3
H-Emanuel <emanuelvperez2000@gmail.com>
2025-02-11 08:54:43 -0300

    cambio_de_asgi

--- web_control_usuario/asgi.py
+++ web_control_usuario/asgi.py
@@ -1,14 +1,16 @@
-import os
 from django.core.asgi import get_asgi_application
+
+django_asgi_app = get_asgi_application()
+
 from channels.routing import ProtocolTypeRouter, URLRouter
-from django.urls import path
-from control_creacion_usuario.routing import websocket_urlpatterns    # Asegúrate de cambiar "tu_app" por el nombre real de tu app
+from channels.auth import AuthMiddlewareStack
+import control_creacion_usuario.routing
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "web_control_usuario.settings")
-
-application = ProtocolTypeRouter(
-    {
-        "http": get_asgi_application(),
-        "websocket": URLRouter(websocket_urlpatterns),  # Habilitar WebSockets
-    }
-)
+application = ProtocolTypeRouter({
+    "http": django_asgi_app,  # Maneja peticiones HTTP normales
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            control_creacion_usuario.routing.websocket_urlpatterns
+        )
+    ),
+})
