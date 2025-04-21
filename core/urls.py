d67b5e1271ad3871ae49285669d58f8108f1c766
buscotoxica <osvaldo.moya@sansano.usm.cl>
2025-04-21 13:01:28 -0400

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.inicio, name="inicio" ),
     path('menu/', views.menu, name="menu"),
-    path('arcgis/',views.arcgisregister,name="arcgis"),
+    path('arcgis/',views.arcgisregister1,name="arcgis"),
     path('SS/',views.SSregistro,name="SS"),
     path('geoportal/',views.geoportalVisita,name="geoportal"),
     path('buscar_protocolo/',views.buscar_protocolo,name="buscar_protocolo"),
