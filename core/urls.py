f650e6e90cf7802e9d01abcbe620961207fac991
buscotoxica <osvaldo.moya@sansano.usm.cl>
2025-04-21 08:36:39 -0400

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -14,5 +14,6 @@ urlpatterns = [
     path('contar_visita_departamentosig/', views.contar_visita_departamentosig, name='contar_visita_departamentosig'),
     path('obtener_visitas_departamentosig/', views.obtener_visitas_departamentosig, name='obtener_visitas_departamentosig'),
     path('guardar_departamento/', views.guardar_departamento, name='guardar_departamento'),
+    path('arcgis1/',views.arcgisregister1,name="arcgis1"),
     
 ]
