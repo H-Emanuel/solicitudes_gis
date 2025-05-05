b08afc65352437b244413f9c5dd86c6311e4d43c
buscotoxica <osvaldo.moya@sansano.usm.cl>
2025-05-05 15:56:51 -0400

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -15,5 +15,7 @@ urlpatterns = [
     path('obtener_visitas_departamentosig/', views.obtener_visitas_departamentosig, name='obtener_visitas_departamentosig'),
     path('guardar_departamento/', views.guardar_departamento, name='guardar_departamento'),
     path('arcgis1/',views.arcgisregister1,name="arcgis1"),
-    
+    path('secpla1/',views.secpla,name="secpla"),
+    path('secpla2/',views.secpla2,name="secpla2"),
+    path('secpla3/',views.secpla3,name="secpla3"),
 ]
